4749133ad78e6cfde39332aa6f0581d3228387b5
Andrew Oh <andrewoh531@gmail.com>
2016-07-31 22:17:30 +1000

    Opening port 3002 in security group of the ec2 instance

--- main.tf
+++ main.tf
@@ -89,6 +89,13 @@ resource "aws_security_group" "ecs_ec2_security_group" {
       cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+      from_port = 3002
+      to_port = 3002
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
