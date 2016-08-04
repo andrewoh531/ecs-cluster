5a430596c699b4eda109d8eb2f62550d97b33c4e
Andrew Oh <andrewoh531@gmail.com>
2016-08-05 08:57:22 +1000

    Adding module for creating an ecs-service

---
+++ modules/ecs-service/variables.tf
@@ -0,0 +1,23 @@
+variable "region" {
+  default = "ap-southeast-2"
+}
+
+variable "ecs_iam_role_name" {}
+
+variable "ecs_iam_role_policy_name" {}
+
+variable "elb_name" {}
+
+variable "container_port" {}
+
+variable "container_name" {}
+
+variable "task_family" {}
+
+variable "container_defintion" {}
+
+variable "ecs_service_name" {}
+
+variable "zone_id" {}
+
+variable "subdomain" {}
