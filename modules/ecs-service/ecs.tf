cb7af9813d76ffb88dbdb34d2e9b08c67a49a2b9
Andrew Oh <andrewoh531@gmail.com>
2016-08-13 19:00:58 +1000

    Adding depends_on attribute on elb to security group

--- modules/ecs-service/ecs.tf
+++ modules/ecs-service/ecs.tf
@@ -65,6 +65,7 @@ resource "aws_security_group" "elb_security_group" {
 
 resource "aws_elb" "elb" {
   name = "${var.elb_name}"
+  depends_on = ["aws_security_group.elb_security_group"]
   availability_zones = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
 
   listener {
