8452d3ac5bc489466136b7b532a4e9e6bef31423
Andrew Oh <andrewoh531@gmail.com>
2016-08-16 18:48:09 +1000

    Give the ec2 instance dynamodb:Query permissions

--- main.tf
+++ main.tf
@@ -31,6 +31,7 @@ resource "aws_iam_role_policy" "ecs_container_role_policy" {
     {
       "Effect": "Allow",
       "Action": [
+        "dynamodb:Query",
         "dynamodb:PutItem",
         "ec2:AuthorizeSecurityGroupIngress",
         "ec2:Describe*",
